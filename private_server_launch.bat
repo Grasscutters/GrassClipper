7bfb9a71135de424ce8273321a1bf0940bb1673e
SpikeHD <spikegdofficial@gmail.com>
2022-04-19 19:22:24 -0700

    wording change

--- private_server_launch.bat
+++ private_server_launch.bat
@@ -1,12 +1,12 @@
 @echo off
 
-echo Starting Private Server
+echo Starting Proxy Server
 
 SET ip=%1
 SET gamePath=%2
 SET gamePath=%gamePath:"=%
 
-start "Proxy Server" ./ext/mitmdump.exe -s "./proxy/proxy.py"
+start "Proxy Server" ./ext/mitmdump.exe -s "./proxy/proxy.py" --ssl-insecure
 
 echo Opening %gamePath%/Genshin Impact Game/GenshinImpact.exe
 
