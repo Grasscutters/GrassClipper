603b919254104e0d2067d72ea62de6374a37bab9
SpikeHD <spikegdofficial@gmail.com>
2022-04-25 19:32:01 -0700

    Fix server launcher

--- scripts/local_server_launch.cmd
+++ scripts/local_server_launch.cmd
@@ -13,15 +13,9 @@ goto l
 set "X=%X:~0,-1%"
 set "GRASSCUTTER_ROOT=%X%"
 
-:: Ensure admin
->nul 2>&1 reg query "HKU\S-1-5-19" || (
-	set params = %*:"="""%
-	cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 "%1"", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-)
-
 echo Starting local Grasscutter server...
 
 :: Change dir to server directory
 cd /d "%GRASSCUTTER_ROOT%"
 
-start /b java -jar %GRASSCUTTER_JAR%
+call java -jar %GRASSCUTTER_JAR%
