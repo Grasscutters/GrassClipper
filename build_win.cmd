23e757d1f94dbd9573b6a7eae42f8394f4e5a988
SpikeHD <spikegdofficial@gmail.com>
2022-04-26 20:50:32 -0700

    use ssl insecure build for testing and ssl secure build for building

--- build_win.cmd
+++ build_win.cmd
@@ -1,8 +1,14 @@
 @echo off
 
+:: This binary is the SSL-secure release
+set BINARY_URL="https://github.com/SpikeHD/neutralinojs/releases/download/v420.69.0/neutralino-win_x64.exe"
+
 :: Clean dist folder
 del /s /q /f .\dist
 rd /s /q .\dist
+
+:: Get the SSL-secure version of the binary
+powershell Invoke-WebRequest -Uri %BINARY_URL% -OutFile "./bin/neutralino-win_x64.exe"
 
 :: build
 call neu build
--- setup_win.cmd
+++ setup_win.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set BINARY_URL="https://github.com/SpikeHD/neutralinojs/releases/latest/download/neutralino-win_x64.exe"
+set BINARY_URL="https://github.com/SpikeHD/neutralinojs/releases/download/v1337.0.0/neutralino-win_x64.exe"
 
 call npm install
 
